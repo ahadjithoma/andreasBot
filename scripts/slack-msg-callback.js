92cbca2febcd1d79ab9f7201c31c0addd02c3c2e
andreash92 <andreas_h92@outlook.com>
2017-05-11 16:08:10 +0300

    update slack-msg-callback.js

--- scripts/slack-msg-callback.js
+++ scripts/slack-msg-callback.js
@@ -1,54 +1,54 @@
-/* VERY IMPORTANT! 
- * If there isn't any verification token against the one on the 
- * slack's app basic info page, anyone can trigger these actions.
- */
-var slackToken = "f";//process.env.HUBOT_SLACK_VERIFY_TOKEN;
+// /* VERY IMPORTANT! 
+//  * If there isn't any verification token against the one on the 
+//  * slack's app basic info page, anyone can trigger these actions.
+//  */
+// var slackToken = "f";//process.env.HUBOT_SLACK_VERIFY_TOKEN;
   
-module.exports = (robot) => {
+// module.exports = (robot) => {
 
 
-  robot.respond(/slack token/i, function(res) {
-    res.reply(slackToken)
-  })
+//   robot.respond(/slack token/i, function(res) {
+//     res.reply(slackToken)
+//   })
 
 
-  robot.router.post('/hubot/slack-msg-callback', (req, res) => {
-    var data = null;
+//   robot.router.post('/hubot/slack-msg-callback', (req, res) => {
+//     var data = null;
 
-    if(req.body.payload) {
-      try {
-        data = JSON.parse(req.body.payload);
-      } catch(e) {
-        robot.logger.error("Invalid JSON submitted to Slack message callback");
-        res.send('You supplied invalid JSON to this endpoint.');
-        //res.send(422)
-        return;
-      }
-    } else {
-      robot.logger.error("Non-JSON submitted to Slack message callback");
-      res.send('You supplied invalid JSON to this endpoint.');
-      //res.send(422)
-      return;
-    }
+//     if(req.body.payload) {
+//       try {
+//         data = JSON.parse(req.body.payload);
+//       } catch(e) {
+//         robot.logger.error("Invalid JSON submitted to Slack message callback");
+//         res.send('You supplied invalid JSON to this endpoint.');
+//         //res.send(422)
+//         return;
+//       }
+//     } else {
+//       robot.logger.error("Non-JSON submitted to Slack message callback");
+//       res.send('You supplied invalid JSON to this endpoint.');
+//       //res.send(422)
+//       return;
+//     }
 
-    if(data.token === slackToken) {
-      robot.logger.info("Request is good");
-    } else {
-      robot.logger.error("Token mismatch on Slack message callback");
-      res.send('You are not authorized to use this endpoint.');
-      //res.send(403)
-      return;
-    }
+//     if(data.token === slackToken) {
+//       robot.logger.info("Request is good");
+//     } else {
+//       robot.logger.error("Token mismatch on Slack message callback");
+//       res.send('You are not authorized to use this endpoint.');
+//       //res.send(403)
+//       return;
+//     }
 
-    var msg = 'slack:msg_action:';
-    var callback_id = data.callback_id;
-    //msg = msg + callback_id;
+//     var msg = 'slack:msg_action:';
+//     var callback_id = data.callback_id;
+//     //msg = msg + callback_id;
     
-    var handled = robot.emit(msg+callback_id, data, res);
-    if (!handled) {
-      //res.send('No scripts handled the action.');
-      //res.send(550)
-      res.send('callback_id: ' + data.callback_id)
-    }
-  });
-};
+//     var handled = robot.emit(msg+callback_id, data, res);
+//     if (!handled) {
+//       //res.send('No scripts handled the action.');
+//       //res.send(550)
+//       res.send('callback_id: ' + data.callback_id)
+//     }
+//   });
+// };
