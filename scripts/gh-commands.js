44928921d1a8bbbf58ae6b7c988525e5de657e3a
andreash92 <andreas_h92@outlook.com>
2017-05-17 13:54:41 +0300

    created gh-commands

---
+++ scripts/gh-commands.js
@@ -0,0 +1,68 @@
+module.exports = function(robot) {
+
+	'use strict';
+	var slackMsgs = require('./slackMsgs.js');				
+
+	/* set Github Account */
+	var GitHubApi = require("github");
+	//var Trello = require(https://api.trello.com/1/client.js?key=51def9cb08cf171cd0970d8607ad8f97);
+
+	var github = new GitHubApi({
+	    /* optional */
+	    // debug: true,
+	    // protocol: "https",
+	    // host: "api.github.com", // should be api.github.com for GitHub
+	    // //thPrefix: "/api/v3", // for some GHEs; none for GitHub
+	    // headers: {
+	    //     "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+	    // },
+	    // Promise: require('bluebird'),
+	    // followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+	    // timeout: 5000
+	});
+
+
+    /* oauth autentication using github personal token */
+	github.authenticate({
+	    "type": "oauth",
+	    "token": process.env.HUBOT_GITHUB_TOKEN
+	})
+
+    /* basic autentication using github's username & password */
+	// github.authenticate({
+	//     type: "basic",
+	//     username: '',
+	//     password: ''
+	// });
+
+
+	robot.respond(/gh followers (.*)/i, function(res_r) {
+		var username = res_r.match[1];	
+		github.users.getFollowersForUser({ 
+			"username": username}, 
+			function(err, res){
+				if (err){
+					res_r.send('Error: ' + JSON.parse(err).message);
+					return false;
+				}
+				var jsonsize = Object.keys(res.data).length;
+
+				let menu = slackMsgs.menu();
+				let login;
+				for (var i = 0; i < jsonsize; i++) {
+					login = res.data[i].login;
+					menu.attachments[0].actions[0]['options'].push({"text":login,"value":login});
+
+      				//TODO: maybe sort them before display
+      			}
+				menu.attachments[0].text = "Followers of "+ "*" + username + "*";      			
+      			menu.attachments[0].fallback = '';
+      			menu.attachments[0].callback_id = 'followers_cb_id';
+      			menu.attachments[0].actions[0].name=' ';
+				menu.attachments[0].actions[0].text=' ';
+
+				res_r.reply(menu);
+			});	
+	})
+
+}
---
+++ scripts/slackMsgs.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+
+    menu: function(){
+        return {
+                "text": "",
+                "mrkdwn": true,
+                "response_type": "in_channel",
+                "attachments": [
+                    {
+                        "text": "",
+                        "fallback": "",
+                        "color": "#3AA3E3",
+                        "attachment_type": "default",
+                        "callback_id": "null",
+                        "mrkdwn_in": [
+                            "text",
+                            "pretext"
+                        ],
+                        "actions": [
+                            {
+                                "name": "",
+                                "text": "",
+                                "type": "select",
+                                "options": [
+                                    {
+                                        "text": "",
+                                        "value": ""
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+    },
+
+    buttons: function(){
+        return {} //TODO
+    }
+
+    
+}
+
+
