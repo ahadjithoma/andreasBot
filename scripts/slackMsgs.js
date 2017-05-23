b597b274a957abf2702c4f536b8c2b0e044d9c1c
andreash92 <andreas_h92@outlook.com>
2017-05-24 01:53:23 +0300

    updaded slackMsgs.js

--- scripts/slackMsgs.js
+++ scripts/slackMsgs.js
@@ -1,5 +1,25 @@
 module.exports = {
 
+    getChannelName: function(robot, res){
+        return (robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(res.message.room)).name;
+    },
+
+    getUserName: function(res){
+        return res.message.user.name;
+    },
+
+    getChannelId: function(res){
+        return  res.message.room;
+    },
+
+    getUserId: function(res){
+        return res.message.user.id;
+    },
+
+    getTeamId: function(res){
+        return res.message.user.team_id;
+    },
+
     menu: function(){
         return {
                 "text": "",
@@ -11,7 +31,7 @@ module.exports = {
                         "fallback": "",
                         "color": "#3AA3E3",
                         "attachment_type": "default",
-                        "callback_id": "null",
+                        "callback_id": "cb_id",
                         "mrkdwn_in": [
                             "text",
                             "pretext"
@@ -23,8 +43,11 @@ module.exports = {
                                 "type": "select",
                                 "options": [
                                     {
-                                        "text": "",
-                                        "value": ""
+                                        // To add menu items: 
+                                        // item = {"text": text, "value": value} 
+                                        // attachements[0].actions.[0].options.push(item)
+                                        // "text": "",
+                                        // "value": ""
                                     }
                                 ]
                             }
@@ -35,26 +58,53 @@ module.exports = {
     },
 
     buttons: function(){
-        return {} //TODO
+        return {
+                "text": "",
+                "attachments": [
+                    {
+                        "text": "",
+                        "fallback": "",
+                        "callback_id": "cb_id",
+                        "mrkdwn_in": [
+                            "text",
+                            "pretext"
+                        ],
+                        "color": "#3AA3E3",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                // To add buttons: 
+                                // item = {"name":name, "text": text, "type":"button", "value":value} 
+                                // attachements[0].actions.push(item)
+                                
+                                // "name": "",
+                                // "text": "",
+                                // "type": "",
+                                // "value": ""
+                            }
+                        ]
+                    }
+                ]
+        } 
     },
 
     basicMessage: function(){
     return {
             "attachments": [
-                { 
+                {
                     "fallback": "",
                     "mrkdwn_in": ["text", "pretext"],
                     "color": "#36a64f",
-                    "pretext": "",
-                    "title": "title",
+                    "pretext": "Please get a token to authorize your Trello account",
+                    "title": "Slack API Documentation",
                     "title_link": "",
-                    "text": "",
-                    "footer": "",
-                    "footer_icon": ""        
-                }
+                    "text": "Copy the token from the link above and run\n *trello add token <YOUR TOKEN>*",
+                    "footer": "Trello Authorization",
+                    "footer_icon": "https://d2k1ftgv7pobq7.cloudfront.net/meta/u/res/images/b428584f224c42e98d158dad366351b0/trello-mark-blue.png"        }
             ]
         }
     }
+
     
 }
 
