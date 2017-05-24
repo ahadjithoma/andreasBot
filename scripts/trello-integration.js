bf15527b17a252a12675677a67edd208776e7261
andreash92 <andreas_h92@outlook.com>
2017-05-24 03:00:36 +0300

    testing robot.on and emit

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -23,6 +23,11 @@ module.exports = function(robot) {
     })
 
 
+    /************** TESTING ************/
+    robot.on('trello_board_list', function(data, res) {
+         res.send('robot.on: TODO');
+    });
+    /***********************************/
 
     /*******************************************************************/
     /*                            BOARDS                               */
