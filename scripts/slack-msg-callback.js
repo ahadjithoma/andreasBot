3ccc8f614cda4e4125f19b56dbbe08bfcc4b18ff
andreash92 <andreas_h92@outlook.com>
2017-05-25 00:35:46 +0300

    update slack callback

--- scripts/slack-msg-callback.js
+++ scripts/slack-msg-callback.js
@@ -39,7 +39,7 @@ module.exports = function(robot)  {
 
     var msg = 'slack:msg_action:'; 
     var callback_id = data.callback_id;
-    console(data);
+    console.log(data);
     var handled = robot.emit(msg+callback_id, data, res);
     if (!handled) {
       //res.send(500)
