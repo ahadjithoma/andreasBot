73df0a7d1b96006441196ee721912ad9acdaff64
andreash92 <andreas_h92@outlook.com>
2017-05-25 02:28:44 +0300

    update slack callback

--- scripts/event-listener.js
+++ scripts/event-listener.js
@@ -12,19 +12,19 @@ module.exports = function(robot) {
 	});
 
 
-  	robot.on(slack_msg + 'trello_board', function(data, res) {
-		res.send('trello board button pressed');
+ //  	robot.on(slack_msg + 'trello_board', function(data, res) {
+	// 	res.send('trello board button pressed');
 	    
-	    var msg = 'trello_board_list'; 
-	    k = res; 
-	    return robot.emit(msg, 'data', k);
-	    //var handled = robot.emit(msg, 'data', res);
-	    // if (!handled) {
-	    //   //res.send(500)
-	    //   res.send('\nNo scripts handled the action.\n');
-	    // }
-		// //console.log(data);
-	});
+	//     var msg = 'trello_board_list'; 
+	//     k = res; 
+	//     return robot.emit(msg, 'data', k);
+	//     //var handled = robot.emit(msg, 'data', res);
+	//     // if (!handled) {
+	//     //   //res.send(500)
+	//     //   res.send('\nNo scripts handled the action.\n');
+	//     // }
+	// 	// //console.log(data);
+	// });
   
   
 }
