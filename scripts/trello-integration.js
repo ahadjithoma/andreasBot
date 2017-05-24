3461f71315349fe4518636b1a2e3368c1f248296
andreash92 <andreas_h92@outlook.com>
2017-05-24 13:33:43 +0300

    update event-listener

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -27,7 +27,7 @@ module.exports = function(robot) {
     /************** TESTING ************/
     robot.on('trello_board_list', function(data, res) {
          //res.send('robot.on: TODO');
-         console.send('\nrobot.on: TODO\n');
+         console.log('\nrobot.on: TODO\n');
     });
     /***********************************/
 
