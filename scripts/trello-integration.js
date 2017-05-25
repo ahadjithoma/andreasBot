6ae596d08c789ebeb6a74b27d96a8c7188303703
andreash92 <andreas_h92@outlook.com>
2017-05-26 02:54:20 +0300

    testing slack msgs

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -96,7 +96,13 @@ module.exports = function(robot) {
         console.log('robot.on: trello_board');
         console.log(data);
         res.send('robot.on: trello_board'); 
+        let value = data.actions[0].value;
+        let btn_name = data.actions[0].name;
+        let cb_id = data.callback_id;
 
+        res.send(value);
+        res.send(cb_id);
+        res.send(btn_name);
     }
 
 
