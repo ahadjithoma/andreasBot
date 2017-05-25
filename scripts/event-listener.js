a3c5eb7a9b4841246488eab6cae770fa7f16efcf
andreash92 <andreas_h92@outlook.com>
2017-05-26 00:28:42 +0300

    testing slack msgs

--- scripts/event-listener.js
+++ scripts/event-listener.js
@@ -1,10 +1,10 @@
 module.exports = function(robot) {
   
 	var slack_msg = 'slack:msg_action:'; 
-  var callback_id = 'wopr_game'; // 'worp_game' is an example for testing purposes
+  	var callback_id = 'wopr_game'; // 'worp_game' is an example for testing purposes
 
-  robot.on(slack_msg + callback_id, function(data, res) {
-		 res.send('robot.on: TODO')
+	robot.on(slack_msg + callback_id, function(data, res) {
+		console.log('robot.on: TODO')
 	});
   
 }
