95f04220772ccf9f129b93debb0c9d61bdd63804
andreash92 <andreas_h92@outlook.com>
2017-05-25 14:07:51 +0300

    testing slack msgs

--- scripts/example-graphical_response.js
+++ scripts/example-graphical_response.js
@@ -3,12 +3,12 @@ module.exports = function(robot) {
   var games_buttons = require('./example-buttons.json');
   var games_menu = require('./example-menu.json');
 
-  robot.respond(/games - buttons/i, function(res) {
-	res.reply(games_buttons)
+  robot.hear(/games - buttons/i, function(res) {
+	res.send(games_buttons)
   })
 
   robot.respond(/games - menu/i, function(res) {
-	res.reply(games_menu)
+	res.send(games_menu)
   })
 
 }
