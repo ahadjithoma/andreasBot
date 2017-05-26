e89982c0d87aa8679a2c0f3f7e176df78bce0ee0
andreash92 <andreas_h92@outlook.com>
2017-05-26 18:27:43 +0300

    update trello integration

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -104,7 +104,7 @@ module.exports = function(robot) {
         let pars = {cards: "all"};
         trelloapi.list_id(listId, pars)
             .then(function(data_list){
-                console.log(listId);
+                console.log(data_board);
                 return data_list;
             })
             .fail(function(err){
