dd65ea6b857a8bf3e0b9b9be9ac384ba677c9b3f
andreash92 <andreas_h92@outlook.com>
2017-05-26 19:40:27 +0300

    update trello integration

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -109,8 +109,8 @@ module.exports = function(robot) {
                 msg.attachments[0].text = `Available Cards`;
                 msg.attachments[0].callback_id = `trello_list`;
                 
-                let cardsNum = Object.keys(data_list.cards);
-                res.send(cardsNum);
+                let cardsNum = Object.keys(data_list.cards).length;
+                console.log(`total cards: ${cardsNum}`);
                 for (var i=0; i<cardsNum; i++){
                     let card    = data_list.cards[i].name;
                     let cardId  = data_list.cards[i].id;
