592f4888bcec400972068cdf77ff1aecde5f6c85
andreash92 <andreas_h92@outlook.com>
2017-06-10 12:57:48 +0300

    update slack-msg0callback

--- scripts/github-hooks.js
+++ scripts/github-hooks.js
@@ -1,10 +1,9 @@
-var slackToken = process.env.HUBOT_SLACK_VERIFY_TOKEN;
 
 module.exports = function(robot)  {
   robot.router.post('/hubot/github-hooks', function(req, res) {
     var data = null;
 
-    console.log(data);
+    console.log(req);
 
   });
 }
