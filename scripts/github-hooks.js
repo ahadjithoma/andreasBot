7e22b504f899901a1a99b4e44eee8008f34edd2d
andreash92 <andreas_h92@outlook.com>
2017-06-10 13:27:21 +0300

    update slack-msg0callback

--- scripts/github-hooks.js
+++ scripts/github-hooks.js
@@ -2,8 +2,25 @@
 module.exports = function(robot)  {
   robot.router.post('/hubot/github-hooks', function(req, res) {
     var data = null;
-    data = req.body;
-    console.log(data);
+
+    if(req.body.payload) {
+      try {
+        data = JSON.parse(req.body.payload);
+      } 
+      catch(e) {
+        robot.logger.error("Invalid JSON submitted to Slack message callback");
+        //res.send(422)
+        res.send('You supplied invalid JSON to this endpoint.');
+        return;
+      }
+    } 
+    else {
+      robot.logger.error("Non-JSON submitted to Slack message callback");
+      //res.send(422)
+      res.send('You supplied invalid JSON to this endpoint.');
+      return;
+    }
+
 
   });
 }
