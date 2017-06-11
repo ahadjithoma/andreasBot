2dfc590eabd4028d24bece4a5ac52eec2485c8c1
andreash92 <andreas_h92@outlook.com>
2017-06-12 01:31:09 +0300

    update gh

--- scripts/github-commands.js
+++ scripts/github-commands.js
@@ -63,7 +63,7 @@ module.exports = function(robot) {
 					let msg = slackMsgs.githubEvent();
 					msg.attachments[0].pretext = `<${repo_url}|[${repo_name}:${branch}]> 1 new commit by ${user_name}:`;
 					msg.attachments[0].title = '';
-					msg.attachments[0].text = `<${commit_url}|`+'`'+`${commit_id}>`+'`'+`${commit_msg} - <www.github.com/${user_login}|${user_name}>`;
+					msg.attachments[0].text = `<${commit_url}|`+'`'+`${commit_id}`>+'`'+`${commit_msg} - <www.github.com/${user_login}|${user_name}>`;
 					robot.messageRoom(room, msg);	
 				} else {
 					robot.messageRoom(room, "push event");	
