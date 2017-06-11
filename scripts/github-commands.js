0a82e532cc6b688dbee7f1caea82bffc32495ef1
andreash92 <andreas_h92@outlook.com>
2017-06-11 16:10:49 +0300

    update gh

--- scripts/github-commands.js
+++ scripts/github-commands.js
@@ -46,7 +46,7 @@ module.exports = function(robot) {
 		adapter = robot.adapterName;
 
 		payload = data.payload;
-		// console.log(payload);
+		console.log(payload.target_url);
 		switch(data.eventType){
 			case 'push': 
 				if (adapter == 'slack'){
