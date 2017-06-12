c20414859f1503003f4f8709025815cf8a00c461
andreash92 <andreas_h92@outlook.com>
2017-06-12 23:35:38 +0300

    update gh

--- scripts/github-commands.js
+++ scripts/github-commands.js
@@ -61,7 +61,7 @@ module.exports = function(robot) {
 			}	
 			msg.attachments[0].pretext = `<${repo_url}|[${repo_name}:${branch}]> <${compare_url}|${commits} new commit(s)> by <www.github.com/${user_login}|${user_name}>:`;
 			msg.attachments[0].color = '#0000ff'; // set color = blue
-			robot.messageRoom(room, msg);
+			robot.messageRoom(room, '```'+msg+'```');
 
 		} else {
 			//TODO: send a msg in plain text for other chat platforms or add any other specific formats than slack's
