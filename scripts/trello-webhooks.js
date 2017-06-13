83e74b1a30920179bcc6c029f905605278c7aac4
andreash92 <andreas_h92@outlook.com>
2017-06-13 19:26:57 +0300

    update gh

--- scripts/trello-webhooks.js
+++ scripts/trello-webhooks.js
@@ -5,22 +5,7 @@ debug = true;
 
 module.exports = function (robot) {
     robot.router.post('/hubot/trello-hooks', function (req, res) {
-        try {
-            if (debug) {
-                robot.logger.info("Trello post received: ", req);
-            }
-            eventBody = {
-                payload: req.body,
-                query: querystring.parse(url.parse(req.url).query)
-            };
-            res.send('json received');
-            robot.emit("trello-webhook-event", eventBody);
-        } catch (e) {
-            // res.send('You supplied invalid JSON to this endpoint.');
-            error = e;
-            // robot.logger.error('Could not receive github response on github-hooks.js');	
-            robot.logger.error("trello-hooks.js error: " + error.stack + "**" + "\n");
-        }
+        res.statusCode(200);
     })
 
 }
