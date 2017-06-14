8e4be0ce4940ed4d7f6dbd1fe662a716b285cfbb
andreash92 <andreas_h92@outlook.com>
2017-06-14 13:52:18 +0300

    update gh

--- scripts/trello-webhooks.js
+++ scripts/trello-webhooks.js
@@ -1,7 +1,8 @@
 
 module.exports = function(robot) {
-  robot.router.post('/hubot/trello-webhooks', function(req, res) {
+  robot.router.head('/hubot/trello-webhooks', function(req, res) {
     robot.logger.info("trello post received: ");
     robot.emit("trello-webhook-event", req, res);
+    res.send(200);
   });
 }
