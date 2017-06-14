1277799daef65033abc938c5d0a66973cd40fa78
andreash92 <andreas_h92@outlook.com>
2017-06-14 15:31:23 +0300

    update gh

--- scripts/trello-webhooks.js
+++ scripts/trello-webhooks.js
@@ -1,8 +1,14 @@
 
-module.exports = function(robot) {
-  robot.router.head('/hubot/trello-webhooks', function(req, res) {
-    robot.logger.info("trello post received: ");
-    robot.emit("trello-webhook-event", req, res);
-    res.send(200);
-  });
+module.exports = function (robot) {
+    robot.router.head('/hubot/trello-webhooks', function (req, res) {
+        robot.logger.info("trello head ");
+        res.send(200);
+    });
+
+
+    robot.router.post('/hubot/trello-webhooks', function (req, res) {
+        robot.logger.info("trello post");
+        robot.emit("trello-webhook-event", req, res);
+        res.send(200);
+    });
 }
