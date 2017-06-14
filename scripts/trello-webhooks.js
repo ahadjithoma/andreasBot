25f5d0c9a46a5c055c4189ef5b44e6353f70dfdf
andreash92 <andreas_h92@outlook.com>
2017-06-14 13:19:48 +0300

    update gh

--- scripts/trello-webhooks.js
+++ scripts/trello-webhooks.js
@@ -1,20 +1,6 @@
-debug = false;
-
-
 module.exports = function(robot) {
   robot.router.post('/hubot/trello-webhooks', function(req, res) {
-	var error;
-	    try {
-	      if (debug) {
-	        robot.logger.info("trello post received: ", req);
-	      }
-	      res.send(200);
-	      robot.emit("trello-webhook-event", req);
-	    } catch (e) {
-		  // res.send('You supplied invalid JSON to this endpoint.');
-	      error = e;
-	      robot.logger.error("trello-hooks.js error: " + error.stack + "**" + "\n");
-	    }
-	    return res.end("");
+    robot.logger.info("trello post received: ");
+    robot.emit("trello-webhook-event", req, res);
   });
 }
