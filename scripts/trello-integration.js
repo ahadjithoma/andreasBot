e144145cf21caf84dab21c1bf998342622f545c8
andreash92 <andreas_h92@outlook.com>
2017-06-21 01:43:31 +0300

    trello webhooks

--- scripts/trello-integration.js
+++ scripts/trello-integration.js
@@ -22,9 +22,10 @@ module.exports = function(robot) {
 
         trello.postAsync('/1/webhooks', args).then(function(data){
             res.send(data)
-        }).catch(function(err){
-            robot.logger.error(err)
-        })
+        });
+        // .catch(function(err){
+        //     robot.logger.error(err)
+        // })
     })
 
 	robot.on('trello-webhook-event', function(data, res){
