28bf0639ffa19b12d05bc6c63da15ad318a319bc
andreash92 <andreas_h92@outlook.com>
2017-06-21 13:35:40 +0300

    trello-itegration & webhooks

--- scripts/github-integration.js
+++ scripts/github-integration.js
@@ -11,15 +11,15 @@ module.exports = function (robot) {
 	var github = new GitHubApi({
 		/* optional */
 		// debug: true,
-		// protocol: "https",
-		// host: "api.github.com", // should be api.github.com for GitHub
-		// //thPrefix: "/api/v3", // for some GHEs; none for GitHub
-		// headers: {
-		//     "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-		// },
-		// Promise: require('bluebird'),
-		// followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-		// timeout: 5000
+		protocol: "https",
+		host: "api.github.com", // should be api.github.com for GitHub
+		thPrefix: "/api/v3", // for some GHEs; none for GitHub
+		headers: {
+			"user-agent": "Hubot-GitHub" // GitHub is happy with a unique user agent
+		},
+		Promise: require('bluebird'),
+		followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+		timeout: 5000
 	});
 
 	/* oauth autentication using github personal token */
@@ -203,34 +203,26 @@ module.exports = function (robot) {
 	})
 
 	// get user's followers. Developed for testing purposes
-	robot.respond(/gh followers (.*)/i, function (res_r) {
+	robot.respond(/gh followers (.*)/, function (res_r) {
 		var username = res_r.match[1];
 		github.users.getFollowersForUser({
 			"username": username
-		},
-			function (err, res) {
-				if (err) {
-					res_r.send('Error: ' + JSON.parse(err).message);
-					return false;
-				}
-				var jsonsize = Object.keys(res.data).length;
-
-				let menu = slackMsgs.menu();
-				let login;
-				for (var i = 0; i < jsonsize; i++) {
-					login = res.data[i].login;
-					menu.attachments[0].actions[0]['options'].push({ "text": login, "value": login });
-
-					//TODO: maybe sort them before display
-				}
-				menu.attachments[0].text = "Followers of " + "*" + username + "*";
-				menu.attachments[0].fallback = '';
-				menu.attachments[0].callback_id = 'followers_cb_id';
-				menu.attachments[0].actions[0].name = ' ';
-				menu.attachments[0].actions[0].text = ' ';
-
-				res_r.reply(menu);
-			});
+		}).then(function (res) {
+			var jsonsize = Object.keys(res.data).length;
+			let menu = slackMsgs.menu();
+			let login;
+			for (var i = 0; i < jsonsize; i++) {
+				login = res.data[i].login;
+				menu.attachments[0].actions[0]['options'].push({ "text": login, "value": login });
+			}
+			menu.attachments[0].text = "Followers of " + "*" + username + "*";
+			menu.attachments[0].fallback = '';
+			menu.attachments[0].callback_id = 'followers_cb_id';
+			menu.attachments[0].actions[0].name = ' ';
+			menu.attachments[0].actions[0].text = ' ';
+			res_r.reply(menu);
+		}).catch(function (err) {
+			res_r.send('Error: ' + JSON.parse(err).message);
+		})
 	})
-
 }
