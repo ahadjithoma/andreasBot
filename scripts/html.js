d5878ca64fb3c9c3e45f22cf97ece4ac74e46f18
andreash92 <andreas_h92@outlook.com>
2017-06-22 00:56:08 +0300

    html

--- scripts/html.js
+++ scripts/html.js
@@ -3,7 +3,8 @@ module.exports = function (robot) {
 
 var html = `
 <input type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
-<button onclick=window.open("https://www.google.com", "popUpWindow")>Try it</button> 
+<button onclick=window.open("https://www.google.com")>open</button>
+<button onclick=window.close()>close</button>  
 `;
 	robot.router.get('/hubot/html', function (req, res) {
         res.send(html);
