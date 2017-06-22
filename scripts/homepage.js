b16ab06b7d6e7d644fb5060a5f0b88cf8edb6dbb
andreash92 <andreas_h92@outlook.com>
2017-06-22 12:56:15 +0300

    add html page

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,7 @@
+var page = require("./homepage.html");
 
 module.exports = function (robot) {
     robot.router.get('/', function (req, res) {
-        res.send(`<h1>Are you looking for something? :)</h1`)
+        res.send(html)
     })
 }
