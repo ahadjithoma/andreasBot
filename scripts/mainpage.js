f54182e8d84366e1dbc4c62b991dbbbf2f3358b2
andreash92 <andreas_h92@outlook.com>
2017-06-22 13:22:49 +0300

    add html page

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -1,11 +1,13 @@
 module.exports = { html: function (){
     
 var k = `<html><body><h2>Hello Human! My name is Hubot</h2><p>Feel free to look around</p><img src="https://camo.githubusercontent.com/ed121c7a3cfcf27c2661490af67ea4aee6305d56/687474703a2f2f692e696d6775722e636f6d2f4e685471655a322e706e67" alt="github logo"><br><br><br>
-<button onClick=window.close()>Close</button><br><script>
+<button onClick=window.close()>Close</button><br>
+
+<p id="demo"></p>
+<script>
 document.getElementById("demo").innerHTML = 
 "The full URL of this page is:<br>" + window.location.href;
 </script>
-
 </body></html>`
 return k;
 }
