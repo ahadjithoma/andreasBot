137f6b7ec3f8880496ae6a297be8ef6f5ccf8bb6
andreash92 <andreas_h92@outlook.com>
2017-06-22 03:09:10 +0300

    update trello token

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,3 +1,6 @@
-	robot.router.get('/', function (req, res) {
+
+module.exports = function (robot) {
+    robot.router.get('/', function (req, res) {
         res.send(`<h1>Are you looking for something? :)</h1`)
-})
+    })
+}
