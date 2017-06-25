8bae0fb0437d4344dc893749e7eee49c71f69256
andreash92 <andreas_h92@outlook.com>
2017-06-26 01:11:18 +0300

    trello token

--- scripts/trello-token.js
+++ scripts/trello-token.js
@@ -46,9 +46,10 @@ module.exports = function(robot) {
         db.collection('trello').find().toArray(function(err, result) {
             if (err) throw err;
             t = result;
+			            console.log(t);
+
         })
 
-            console.log(t);
 
         let boardId = 'BE7seI7e';
         let args = { fields: "name,url,prefs" };
