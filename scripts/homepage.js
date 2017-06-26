29f98570c21124572f2d880c892d1c9069392dee
andreash92 <andreas_h92@outlook.com>
2017-06-26 03:28:39 +0300

    testing html

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -6,8 +6,18 @@ module.exports = function (robot) {
     })
 
     robot.router.get("/a", function (request, response) {  
- response.send(`<h2>Token succesfuly received. You can now close the window.</h2>\n
-    				<button onclick=window.close()>close</button>`)
+
+        response.send(`
+        <html><body>
+        
+        <script type="text/javascript">
+            window.parent.close();
+        </script>
+        
+        </body></html>
+        `);
+//  response.send(`<h2>Token succesfuly received. You can now close the window.</h2>\n
+    				// <button onclick=window.close()>close</button>`)
 
 });
 }
