cc4cc02655828737fd21cbbc290aca9e0aaec3fb
andreash92 <andreas_h92@outlook.com>
2017-06-27 15:35:02 +0300

    tl

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,16 +1,21 @@
-var trello = require('node-trello');
+var Trello = require('node-trello');
 var bcrypt = require('bcryptjs');
 var db = require('./mlab-login.js').db();
 
+var trello = {};
+
 db.bind('trelloTokens');
 db.trelloTokens.find().toArrayAsync()
-.then(function(records){
-    let length = Object.keys(records).length; 
-    let i = 0;
-    for (i=0; i<length; i++){
-        console.log(records[i].id);
-    }
-})
-.catch(error => {
-    console.log(error)
-})
+    .then(function (records) {
+        let length = Object.keys(records).length;
+        let i = 0;
+        for (i = 0; i < length; i++) {
+            bcrypt.compare('', records[i].token).then(function (res) {
+                console.log(res);
+            });
+
+        }
+    })
+    .catch(error => {
+        console.log(error)
+    })
