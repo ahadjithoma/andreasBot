7289262adc27ad125ccde8e321c04229253c881a
andreash92 <andreas_h92@outlook.com>
2017-06-27 14:56:14 +0300

    tl

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -3,7 +3,7 @@ var bcrypt = require('bcryptjs');
 var db = require('./mlab-login.js').db();
 
 db.bind('trelloTokens');
-db.trelloTokens.find().toArray()
+db.trelloTokens.find().toArrayAsync()
 .then(function(result){
     console.log(result);
 })
