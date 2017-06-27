a028b0fea85501ca5c5b84f32a4c2eb447af1a31
andreash92 <andreas_h92@outlook.com>
2017-06-27 19:43:09 +0300

    tl

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -2,29 +2,21 @@
 
 const crypto = require('crypto');
 
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bytes (32 characters)
-const IV_LENGTH = 16; // For AES, this is always 16
+const key = process.env.ENCRYPTION_KEY; // Must be   256 bytes (32 characters)
+const algorithm = 'aes-256-ctr';
 
 function encrypt(text) {
-  let iv = 'EOQePbyc0TJC3AMt';//crypto.randomBytes(IV_LENGTH);
-  let cipher = crypto.createCipheriv('aes-256-ctr', new Buffer(ENCRYPTION_KEY), iv);
-  let encrypted = cipher.update(text);
-
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-  return iv.toString('hex') + ':' + encrypted.toString('hex');
+  var cipher = crypto.createCipher(algorithm, key)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
 }
 
 function decrypt(text) {
-  let textParts = text.split(':');
-  let iv = 'EOQePbyc0TJC3AMt';//new Buffer(textParts.shift(), 'hex');
-  let encryptedText = new Buffer(textParts.join(':'), 'hex');
-  let decipher = crypto.createDecipheriv('aes-256-ctr', new Buffer(ENCRYPTION_KEY), iv);
-  let decrypted = decipher.update(encryptedText);
-
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-  return decrypted.toString();
+  var decipher = crypto.createDecipher(algorithm, key)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
 }
 
 module.exports = { decrypt, encrypt };
