3d85d32d579f1dd6e017bfbfb044614eae7b0415
andreash92 <andreas_h92@outlook.com>
2017-06-27 18:40:08 +0300

    tl

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -7,7 +7,7 @@ const IV_LENGTH = 16; // For AES, this is always 16
 
 function encrypt(text) {
   let iv = 'EOQePbyc0TJC3AMt';//crypto.randomBytes(IV_LENGTH);
-  let cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+  let cipher = crypto.createCipheriv('aes-256-ctr', new Buffer(ENCRYPTION_KEY), iv);
   let encrypted = cipher.update(text);
 
   encrypted = Buffer.concat([encrypted, cipher.final()]);
@@ -19,7 +19,7 @@ function decrypt(text) {
   let textParts = text.split(':');
   let iv = 'EOQePbyc0TJC3AMt';//new Buffer(textParts.shift(), 'hex');
   let encryptedText = new Buffer(textParts.join(':'), 'hex');
-  let decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+  let decipher = crypto.createDecipheriv('aes-256-ctr', new Buffer(ENCRYPTION_KEY), iv);
   let decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([decrypted, decipher.final()]);
