5eb6ce64f7d16066b0179e8bc37fd51e9b687b66
andreash92 <andreas_h92@outlook.com>
2017-06-27 14:52:49 +0300

    tl

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,7 +1,8 @@
 var trello = require('node-trello');
 var bcrypt = require('bcryptjs');
-var db = require('./mlab-login.js').db().bind('trelloTokens');
+var db = require('./mlab-login.js').db();
 
+db.bind('trelloTokens');
 db.trelloTokens.find().toArray()
 .then(function(result){
     console.log(result);
