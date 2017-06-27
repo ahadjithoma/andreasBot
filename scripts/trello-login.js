1cc2640f4d9300cd89881c532b6f4c5ec1c668b8
andreash92 <andreas_h92@outlook.com>
2017-06-27 15:02:12 +0300

    tl

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -4,8 +4,12 @@ var db = require('./mlab-login.js').db();
 
 db.bind('trelloTokens');
 db.trelloTokens.find().toArrayAsync()
-.then(function(result){
-    console.log(result);
+.then(function(records){
+    let length = Object.keys(records).length; 
+    let i = 0;
+    for (i=0; i<length; i++){
+        console.log(records[i].id);
+    }
 })
 .catch(error => {
     console.log(error)
