f2744c0ef690586a52e91b3159b91ae1cf29ece1
andreash92 <andreas_h92@outlook.com>
2017-06-29 01:53:25 +0300

    trello login

--- scripts/TEST-trello-api.js
+++ scripts/TEST-trello-api.js
@@ -44,7 +44,7 @@ module.exports = function (robot) {
 
 		let userId = msg.getUserId(res);
 		let trello = t.trelloLogin;
-		console.log(trello.trelloLogin(userId));
+		console.log(trello(userId));
 		trello.trelloLogin(userId).get('/1/members/me', function (err, data) {
 			res.send('promise' + data.fullName);
 		})
