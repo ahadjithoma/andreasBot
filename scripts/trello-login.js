686a7a0f2f0bda32cc333a135a528c5338c1aa44
andreash92 <andreas_h92@outlook.com>
2017-06-29 01:12:35 +0300

    trello login

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,61 +1,61 @@
-module.exports = function trelloLogin(userID) {
+// module.exports = function trelloLogin(userID) {
 
-    var Trello = require('node-trello');
-    var encryption = require('./encryption.js');
-    var Promise = require("bluebird");
-    var mongo = require('mongoskin');
-    // mLab connection URI
-    var uri = process.env.MONGODB_URI;
-    Promise.promisifyAll(mongo);
-    // connect to mLab database
-    var db = mongo.MongoClient.connect(uri);
+//     var Trello = require('node-trello');
+//     var encryption = require('./encryption.js');
+//     var Promise = require("bluebird");
+//     var mongo = require('mongoskin');
+//     // mLab connection URI
+//     var uri = process.env.MONGODB_URI;
+//     Promise.promisifyAll(mongo);
+//     // connect to mLab database
+//     var db = mongo.MongoClient.connect(uri);
 
-    var key = process.env.HUBOT_TRELLO_KEY;
-    var trello = {};
+//     var key = process.env.HUBOT_TRELLO_KEY;
+//     var trello = {};
 
 
-    // db.bind('trelloTokens');
-    // db.trelloTokens.find().toArrayAsync()
-    //     .then(function (records) {
-    //         let length = Object.keys(records).length;
-    //         let i = 0;
-    //         console.log(`lenght = ${length}`);
-    //         for (i = 0; i < length; i++) {
-    //             let token = encryption.decrypt(records[i].token);
-    //             let userId = records[i].id;
-    //             let username = records[i].username;
-    //             let t = new Trello(key, token);
-    //             trello[userId] = Promise.promisifyAll(t);
-    //         }
+//     // db.bind('trelloTokens');
+//     // db.trelloTokens.find().toArrayAsync()
+//     //     .then(function (records) {
+//     //         let length = Object.keys(records).length;
+//     //         let i = 0;
+//     //         console.log(`lenght = ${length}`);
+//     //         for (i = 0; i < length; i++) {
+//     //             let token = encryption.decrypt(records[i].token);
+//     //             let userId = records[i].id;
+//     //             let username = records[i].username;
+//     //             let t = new Trello(key, token);
+//     //             trello[userId] = Promise.promisifyAll(t);
+//     //         }
 
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //     })
+//     //     })
+//     //     .catch(error => {
+//     //         console.log(error)
+//     //     })
 
-    var trello = {};
+//     var trello = {};
 
-    // db.bind('trelloTokens');
-    db.collection('trelloTokens').find({ id: userID }).toArray(function (err, data) {
-        if (err) throw err;
-        let token = encryption.decrypt(data.token);
-        let userId = data.id;
-        let username = data.username;
-        let t = new Trello(key, token);
-        trello = Promise.promisifyAll(t);
-    })
+//     // db.bind('trelloTokens');
+//     db.collection('trelloTokens').find({ id: userID }).toArray(function (err, data) {
+//         if (err) throw err;
+//         let token = encryption.decrypt(data.token);
+//         let userId = data.id;
+//         let username = data.username;
+//         let t = new Trello(key, token);
+//         trello = Promise.promisifyAll(t);
+//     })
 
-    // db.bind('trelloTokens');
-    // db.trelloTokens.find({ id: userID }).toArrayAsync()
-    //     .then(function (data) {
-    //         let token = encryption.decrypt(data.token);
-    //         let userId = data.id;
-    //         let username = data.username;
-    //         let t = new Trello(key, token);
-    //         trello = Promise.promisifyAll(t);
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //     })
-    return trello;
-}
+//     // db.bind('trelloTokens');
+//     // db.trelloTokens.find({ id: userID }).toArrayAsync()
+//     //     .then(function (data) {
+//     //         let token = encryption.decrypt(data.token);
+//     //         let userId = data.id;
+//     //         let username = data.username;
+//     //         let t = new Trello(key, token);
+//     //         trello = Promise.promisifyAll(t);
+//     //     })
+//     //     .catch(error => {
+//     //         console.log(error)
+//     //     })
+//     return trello;
+// }
