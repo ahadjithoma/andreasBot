0c6266baaf31c874b9b38d378e9f28912b74b1e9
andreash92 <andreas_h92@outlook.com>
2017-06-28 23:51:46 +0300

    trello login

--- scripts/trello-token.js
+++ scripts/trello-token.js
@@ -7,6 +7,9 @@ var request = require('request-promise');
 var encryption = require('./encryption.js');
 db.bind('trelloTokens');
 
+var dbRefresh = require('./mlab-login.js');
+var trelloRefresh = require('./trello-login.js');
+
 var app_key = process.env.HUBOT_TRELLO_KEY;
 var oauth_secret = process.env.HUBOT_TRELLO_OAUTH;
 
@@ -46,6 +49,8 @@ module.exports = function (robot) {
                 if (err) throw err;
                 if (result) {
                     robot.logger.info(`User's Token Added to DB!`)
+                    dbRefresh;
+                    trelloRefresh;
                 };
             })
         })
