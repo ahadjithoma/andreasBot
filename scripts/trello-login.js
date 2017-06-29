52c32c4ebb26c1513b4781f9a3ddef4bb102e873
andreash92 <andreas_h92@outlook.com>
2017-06-29 20:17:56 +0300

    trello login

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,10 +1,26 @@
 var Trello = require('node-trello');
 var encryption = require('./encryption.js');
-var db = require('./mlab-login.js').db();
 var Promise = require("bluebird");
-
 var key = process.env.HUBOT_TRELLO_KEY;
 var trello = {};
+var mongo = require('mongoskin');
+var Promise = require("bluebird");
+
+// mLab connection URI
+var uri = process.env.MONGODB_URI; 
+
+// promisify mongoskin with bluebird
+Object.keys(mongo).forEach(function (key) {
+    var value = mongo[key];
+    if (typeof value === "function") {
+        Promise.promisifyAll(value);
+        Promise.promisifyAll(value.prototype);
+    }
+});
+Promise.promisifyAll(mongo);
+
+// connect to mLab database
+var db = mongo.MongoClient.connect(uri);
 
 db.bind('trelloTokens');
 db.trelloTokens.find().toArrayAsync()
