bda134cec28b3199a668c25e655eae090aaef3a9
andreash92 <andreas_h92@outlook.com>
2017-06-29 15:32:33 +0300

    trello login

--- scripts/TEST-trello-api.js
+++ scripts/TEST-trello-api.js
@@ -29,24 +29,9 @@ module.exports = function (robot) {
 
 		}).catch(function (err) {
 		})
-		console.log('1');
-		console.log('2');
-		console.log('3');
-		console.log('4');
-		console.log('5');
-		console.log('6');
-		console.log('7');
-		console.log('8');
-		console.log('9');
-		console.log('10');
-		console.log('11');
-		console.log('12');
-		console.log('13');
-		console.log('14');
-		console.log('15');
-		console.log(t['token']);
-		return new Trello(key, token);
 
+		console.log(t['token']);
+		return new Trello(key, t['token']);
 	}
 
 	// in some way CHECK TOKEN VALIDATION
