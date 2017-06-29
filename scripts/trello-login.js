e3c1da98bd855b246f35ad2d466e474ff9353d23
andreash92 <andreas_h92@outlook.com>
2017-06-30 02:54:22 +0300

    testing

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,5 +1,18 @@
+var mongo = require('mongoskin');
+// mLab connection URI
+var uri = process.env.MONGODB_URI;
+// promisify mongoskin with bluebird
+Object.keys(mongo).forEach(function (key) {
+    var value = mongo[key];
+    if (typeof value === "function") {
+        Promise.promisifyAll(value);
+        Promise.promisifyAll(value.prototype);
+    }
+});
+Promise.promisifyAll(mongo);
 
-module.exports = function(userId) {
+
+module.exports = function (userId) {
     var Trello = require('node-trello');
     var encryption = require('./encryption.js');
     var Promise = require("bluebird");
@@ -8,25 +21,14 @@ module.exports = function(userId) {
     var q = require('q')
     var deferred = q.defer();
 
-    var mongo = require('mongoskin');
-    // mLab connection URI
-    var uri = process.env.MONGODB_URI;
-    // promisify mongoskin with bluebird
-    Object.keys(mongo).forEach(function (key) {
-        var value = mongo[key];
-        if (typeof value === "function") {
-            Promise.promisifyAll(value);
-            Promise.promisifyAll(value.prototype);
-        }
-    });
-    Promise.promisifyAll(mongo);
+
     // connect to mLab database
     var db = mongo.MongoClient.connect(uri);
 
 
     // var db = require('./mlab-login').db();
 
-    db.collectionAsync('trelloTokens').find({ id: userId }).toArrayAsync()
+    db.collection('trelloTokens').find({ id: userId }).toArrayAsync()
         .then(function (dbData) {
             console.log(dbData)
             var decryptedToken = dbData[0].token;
