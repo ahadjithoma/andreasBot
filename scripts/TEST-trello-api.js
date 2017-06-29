b6a33a77120fa4d2b1cbec5e424d33068ec83af5
andreash92 <andreas_h92@outlook.com>
2017-06-29 04:29:17 +0300

    trello login

--- scripts/TEST-trello-api.js
+++ scripts/TEST-trello-api.js
@@ -24,10 +24,10 @@ module.exports = function (robot) {
 		db.bind('trelloTokens');
 		db.trelloTokens.find({id:userId}).toArray(function(err, data){
 			console.log(data);
-			t[0] = data.token;
-			console.log(t);
+			t['token'] = data.token;
+			console.log(t['token']);
 		})
-		console.log(t[0]);
+		console.log(t['token']);
 		return new Trello(key, token);
 	}
 
