0198a1baf142e308e050c8fa9f41b6e8e9cfbd93
andreash92 <andreas_h92@outlook.com>
2017-06-30 02:52:53 +0300

    testing

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -26,7 +26,7 @@ module.exports = function(userId) {
 
     // var db = require('./mlab-login').db();
 
-    db.collection('trelloTokens').find({ id: userId }).toArrayAsync()
+    db.collectionAsync('trelloTokens').find({ id: userId }).toArrayAsync()
         .then(function (dbData) {
             console.log(dbData)
             var decryptedToken = dbData[0].token;
