7b65bc20550de0d18ed3411e784e4eb0eca618de
andreash92 <andreas_h92@outlook.com>
2017-06-30 14:42:55 +0300

    testing

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -25,24 +25,23 @@ module.exports = function (userId) {
     // connect to mLab database
     var db = mongo.MongoClient.connect(uri);
 
-
     // var db = require('./mlab-login').db();
     var collection = db.collection('trelloTokens');
-    collection.find({ id: userId }).toArray(function (dbError, dbData) {
-        if (dbError) {
-            console.log(dbError);
-            deferred.reject(dbError);
-        } else {
-            console.log(dbData)
-            var decryptedToken = dbData[0].token;
-            console.log(decryptedToken);
-            let token = encryption.decrypt(decryptedToken);
-            let userId = dbData[0].id;
-            let username = dbData[0].username;
-            let t = new Trello(key, token);
-            var trello = Promise.promisifyAll(t);
-            deferred.resolve(trello);
-        }
-    })
-    return deferred.promise;
+    // collection.find({ id: userId }).toArray(function (dbError, dbData) {
+    //     if (dbError) {
+    //         console.log(dbError);
+    //         deferred.reject(dbError);
+    //     } else {
+    //         console.log(dbData)
+    //         var decryptedToken = dbData[0].token;
+    //         console.log(decryptedToken);
+    //         let token = encryption.decrypt(decryptedToken);
+    //         let userId = dbData[0].id;
+    //         let username = dbData[0].username;
+    //         let t = new Trello(key, token);
+    //         var trello = Promise.promisifyAll(t);
+    //         deferred.resolve(trello);
+    //     }
+    // })
+    // return deferred.promise;
 }
