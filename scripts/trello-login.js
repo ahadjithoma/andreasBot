b130953ed21a0a738fedf2bceb6e2419ebe52042
andreash92 <andreas_h92@outlook.com>
2017-06-30 19:53:06 +0300

    testing

--- scripts/trello-login.js
+++ scripts/trello-login.js
@@ -1,4 +1,3 @@
-
 var Trello = require('node-trello');
 var encryption = require('./encryption.js');
 var Promise = require("bluebird");
@@ -13,20 +12,16 @@ module.exports.trelloLogin = function (userId) {
     // bind trelloTokens collection
     db.bind('trelloTokens');
 
-    db.trelloTokens.findOne({ id: userId }).then(dbData => {
-        console.log(dbData)
+    db.trelloTokens.findOneAsync({ id: userId }).then(dbData => {
         var decryptedToken = dbData.token;
-        let token = encryption.decrypt(decryptedToken);
-        let userId = dbData.id;
-        let username = dbData.username;
-        let t = new Trello(key, token);
+        var token = encryption.decrypt(decryptedToken);
+        var t = new Trello(key, token);
         var trello = Promise.promisifyAll(t);
         deferred.resolve(trello);
     }).catch(dbError => {
         console.log(dbError);
         deferred.reject(dbError);
     })
-
     // return the promise
-    return deferred.promise;
+    return deferred.promise; 
 }
