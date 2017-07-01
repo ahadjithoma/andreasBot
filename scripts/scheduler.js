a178d5245d6ecb426752f01eee95781ecaefc579
andreash92 <andreas_h92@outlook.com>
2017-07-01 19:16:52 +0300

    cron

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -7,29 +7,32 @@ module.exports = function (robot) {
     var db = require('./mlab-login.js').db();
     var encryption = require('./encryption.js');
     var CronJob = require('cron').CronJob;
-    var job = new CronJob('00 54 14 * * *', function () {
-        db.bind('trelloTokens');
-        db.trelloTokens.find().toArrayAsync().then(dbData => {
-            var num = dbData.length;
-            for (let i = 0; i < num; i++) {
-                var encryptedToken = dbData[i].token;
-                var userId = dbData[i].id;
-                var token = encryption.decrypt(encryptedToken);
-                var trello = Promise.promisifyAll(new Trello(key, token));
-                trello.getAsync('/1/member/me/notifications').then(trData => {
+    var job = new CronJob('00 54 14 * * *',     trelloNotifications()
 
-                    robot.logger.info(trData.data);
-                }).catch(trError => {
-                    robot.logger.error(trError);
-                })
-            }
-        }).catch(dbError => {
+    // function () {
+        // db.bind('trelloTokens');
+        // db.trelloTokens.find().toArrayAsync().then(dbData => {
+        //     var num = dbData.length;
+        //     for (let i = 0; i < num; i++) {
+        //         var encryptedToken = dbData[i].token;
+        //         var userId = dbData[i].id;
+        //         var token = encryption.decrypt(encryptedToken);
+        //         var trello = Promise.promisifyAll(new Trello(key, token));
+        //         trello.getAsync('/1/member/me/notifications').then(trData => {
 
-        })
-        robot.messageRoom('general', 'job started');
-        console.log('cron job B STARTED')
+        //             robot.logger.info(trData.data);
+        //         }).catch(trError => {
+        //             robot.logger.error(trError);
+        //         })
+        //     }
+        // }).catch(dbError => {
 
-    }, function () {
+        // })
+        // robot.messageRoom('general', 'job started');
+        // console.log('cron job B STARTED')
+
+    // }
+    , function () {
         robot.messageRoom('general', 'job stopped');
         /* This function is executed when the job stops */
     },
@@ -40,7 +43,6 @@ module.exports = function (robot) {
     // if (db.somewhere == false) { job.stop() }
 
 
-    trelloNotifications();
     /********************* THIS SHOULD GO TO SCHEDULER****************************/
     function trelloNotifications() {
         db.bind('trelloTokens');
