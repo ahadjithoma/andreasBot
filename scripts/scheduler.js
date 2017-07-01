69503da2d424064fb4ba70b4b9809af81ef887ff
andreash92 <andreas_h92@outlook.com>
2017-07-01 14:27:49 +0300

    cron

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -1,29 +1,34 @@
+var key = process.env.HUBOT_TRELLO_KEY;
+var Promise = require('bluebird');
+var Trello = require('node-trello');
+
 module.exports = function (robot) {
     var db = require('./mlab-login.js').db();
-
-    db.bind('trelloTokens');
-    db.trelloTokens.find().toArrayAsync().then(dbData => {
-        var num = dbData.length;
-
-    }).catch(dbError => {
-
-    })
-
-
-
+    var encryption = require('./encryption.js');
     var CronJob = require('cron').CronJob;
     var job = new CronJob('00 00 11 * * *', function () {
+        db.trelloTokens.find().toArrayAsync().then(dbData => {
+            var num = dbData.length;
+            for (let i = 0; i < num; i++) {
+                var encryptedToken = dbData[i].token;
+                var userId = dbData[i].id;
+                var token = encryption.decrypt(encryptedToken);
+                var trello = Promise.promisifyAll(new Trello(key, token));
+
+                trello.getAsync('/1/member/me').then(trData => {
+                    robot.logger.info(trData);
+                }).catch(trError => {
+                    robot.logger.error(trError);
+                })
+            }
+        }).catch(dbError => {
+
+        })
         robot.messageRoom('general', 'job started');
         console.log('cron job B STARTED')
-        job.stop();
-        /*
-         * Runs every weekday (Monday through Friday)
-         * at 11:30:00 AM. It does not run on Saturday
-         * or Sunday.
-         */
+
     }, function () {
         robot.messageRoom('general', 'job stopped');
-        console.log('cron job B FINISHED')
         /* This function is executed when the job stops */
     },
         true, /* Start the job right now */
