e34d491ecc599d9a4920699cae4634db4411f104
andreash92 <andreas_h92@outlook.com>
2017-07-02 14:22:45 +0300

    trello scheduler

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -44,7 +44,8 @@ module.exports = function (robot) {
 
                     for (let j = 0; j < notifNum; j++) { // j: the number of notifications per user
                         let attachment = message.attachment()
-                        attachment.text = notif[j].type.split(/(?=[A-Z])/).join(" ").toLowerCase(); // split capitals, join and convert to lowercase 
+                        attachment.text = notif[j].type.split(/(?=[A-Z])/).join(" ").toLowerCase() // split capitals, join and convert to lowercase 
+                                        + 'by ' + notif[j].memberCreator.username;  
                         msg.attachments.push(attachment);
                     }
                     
