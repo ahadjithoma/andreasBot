81adf5806ae9ac40ff560e6808da967fc3e7c64d
andreash92 <andreas_h92@outlook.com>
2017-07-06 00:16:58 +0300

    trello-webhooks

--- scripts/trello-webhooks.js
+++ scripts/trello-webhooks.js
@@ -6,43 +6,60 @@ module.exports = function (robot) {
     });
 
     robot.router.post('/hubot/trello-webhooks', function (req, res) {
-        let headers = JSON.stringify(req.headers);  
+        let headers = JSON.stringify(req.headers);
         robot.logger.info(`trello-webhook POST. Status Code: ${res.statusCode}\nHeaders: ${headers}`);
-        robot.emit("trello-webhook-event", req, res);
+        // robot.emit("trello-webhook-event", req, res);
         res.send(200);
 
         var room = "random";
         let payload = req.body;
         let type = payload.action.type;
-        // switch (type)
+        robot.logger.info(payload); // payload sample here: https://github.com/andreash92/trello-webhooks
+        switch (type) {
+            case 'addAttachmentToCard':
+            case 'addChecklistToCard':
+            case 'addLabelToCard':
+            case 'addMemberToBoard':
+            case 'addMemberToCard':
+            case 'commentCard':
+            case 'convertToCardFromCheckItem':
+            case 'copyCard':
+            case 'createCard':
+            case 'createLabel':
+            case 'createCheckItem':
+            case 'createLabel':
+            case 'createList':
+            case 'deleteAttachmentFromCard':
+            case 'deleteCard':
+            case 'deleteCheckItem':
+            case 'deleteComment':
+            case 'deleteLabel':
+            case 'emailCard':
+            case 'moveCardFromBoard':
+            case 'moveCardToBoard':
+            case 'moveListFromBoard':
+            case 'moveListToBoard':
+            case 'removeChecklistFromCard':
+            case 'removeLabelFromCard':
+            case 'removeMemberFromBoard':
+            case 'removeMemberFromCard':
+            case 'updateBoard':
+            case 'updateCard':
+            case 'updateCheckItem':
+            case 'updateCheckItemStateOnCard':
+            case 'updateChecklist':
+            case 'updateComment':
+            case 'updateLabel':
+            case 'updateList':
+            case 'voteOnCard':
+                robot.messageRoom(room, type.split(/(?=[A-Z])/).join(" ").toLowerCase());
+                break;
+            default:
+                robot.messageRoom(room, 'webhook not found: ' + type.split(/(?=[A-Z])/));
+                break;
+        }
         robot.messageRoom('random', type);
 
-/*addAdminToBoard
-addAdminToOrganization
-addedAttachmentToCard
-addedMemberToCard
-addedToBoard
-addedToCard
-addedToOrganization
-cardDueSoon
-changeCard
-closeBoard
-commentCard
-createdCard
-declinedInvitationToBoard
-declinedInvitationToOrganization
-invitedToBoard
-invitedToOrganization
-makeAdminOfBoard
-makeAdminOfOrganization
-memberJoinedTrello
-mentionedOnCard
-removedFromBoard
-removedFromCard
-removedFromOrganization
-removedMemberFromCard
-unconfirmedInvitedToBoard
-unconfirmedInvitedToOrganization
-updateCheckItemStateOnCard*/
+
     });
 }
