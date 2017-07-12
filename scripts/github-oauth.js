e8a5c4b1f5959b02207e823a689bda338a39103f
andreash92 <andreas_h92@outlook.com>
2017-07-12 12:52:27 +0300

    github oauth

--- scripts/github-oauth.js
+++ scripts/github-oauth.js
@@ -18,17 +18,31 @@ module.exports = function (robot) {
     // Initialize the OAuth2 Library
     const oauth2 = require('simple-oauth2').create(credentials);
 
+    // Authorization oauth2 URI
+    const authorizationUri = oauth2.authorizationCode.authorizeURL({
+        redirect_uri: 'https://andreasbot.herokuapp.com/hubot/github-oauth',
+        scope: 'repos',
+        state: 'state'
+    });
 
-    const tokenConfig = {};
+    // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
+    res.redirect(authorizationUri);
 
-    // Get the access token object for the client
-    oauth2.clientCredentials.getToken(tokenConfig, (error, result) => {
+    // Get the access token object (the authorization code is given from the previous step).
+    const tokenConfig = {
+        code: '<code>',
+        redirect_uri: 'https://andreasbot.herokuapp.com/hubot/github-oauth'
+    };
+
+    // Callbacks
+    // Save the access token
+    oauth2.authorizationCode.getToken(tokenConfig, (error, result) => {
         if (error) {
             return robot.logger.error('Access Token Error', error.message);
         }
 
         const token = oauth2.accessToken.create(result);
-        robot.logger.info(token)
+        robot.logger.info(token);
     });
 
 }
