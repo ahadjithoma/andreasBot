7e90f80e5b94e0200b0d742cdc7cf64a42f41600
andreash92 <andreas_h92@outlook.com>
2017-08-06 12:49:52 +0300

    trello oauth - trello token testing

--- scripts/trello-token.js
+++ scripts/trello-token.js
@@ -59,7 +59,7 @@ module.exports = function (robot) {
                 //TODO error
                 var trelloUsername = data.username
                 db.bind('users');
-                db.users.update({ _id: userId},{'$push':{trello_username: trelloUsername, trello_token: token }}, function (err, result) {
+                db.users.update({ _id: userId},{'$addToSet':{trello_username: trelloUsername, trello_token: token }}, function (err, result) {
                     if (err) throw err;
                     if (result) {
                         robot.logger.info(`User's Token Added to DB!`)
