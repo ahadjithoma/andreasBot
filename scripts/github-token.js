40931a2490937b65e4ab1880055fa596c2435ee5
andreash92 <andreas_h92@outlook.com>
2017-08-14 03:56:01 +0300

    gh token.js

--- scripts/github-token.js
+++ scripts/github-token.js
@@ -8,7 +8,13 @@ var token_url = 'https://github.com/login/oauth/access_token'
 var bot_host = process.env.HUBOT_HOST_URL
 var GitHubApi = require('github')
 var cache = require('./cache.js').getCache()
-
+var oauth = require("oauth").OAuth2;
+var OAuth2 = new oauth(
+    client_id,
+    client_secret,
+    "https://github.com/",
+    "login/oauth/authorize",
+    "login/oauth/access_token");
 
 module.exports = (robot) => {
 
