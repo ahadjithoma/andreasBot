176400223079a6d8d4376c0d98b5aaa4e7c617de
andreash92 <andreas_h92@outlook.com>
2017-08-14 03:50:11 +0300

    gh-token.js

---
+++ scripts/github-token.js
@@ -0,0 +1,69 @@
+ robot.router.get('/auth/github/callback', function (req, res) {
+        var userid = JSON.parse(req.query.state).userid;
+        var username = JSON.parse(req.query.state).username;
+
+        var code = req.query.code;
+
+        OAuth2.getOAuthAccessToken(code, {}, function (err, access_token) {
+            if (err) {
+                console.log(err);
+            }
+
+            // ****
+            // TODO:
+            // BETTER Handling of promises!!
+            // ****
+
+            var db = require('./mlab-login.js').db();
+
+            github.authenticate({
+                "type": "token",
+                "token": access_token
+            })
+            github.users.get({}, function (err, res) {
+                var github_username = res.data.login
+                db.bind('users').findAndModifyAsync(
+                    { _id: userid },
+                    [["_id", 1]],
+                    { $set: { github_username: github_username } },
+                    { upsert: true })
+                    .then(res => { })
+                    .catch(err => {
+                        robot.logger.error(err)
+                        if (c.errorsChannel) {
+                            robot.messageRoom(c.errorsChannel, c.errorMessage
+                                + `Script: ${path.basename(__filename)}`)
+                        }
+                    })
+                var values = {
+                    github_username: github_username,
+                    github_token: access_token
+                }
+                cache.set(userid, values)
+            })
+            encryption.encrypt(access_token).then(encryptedToken => {
+
+                //TODO -> convert to promise
+                db.bind('users').findAndModifyAsync(
+                    { _id: userid },
+                    [["_id", 1]],
+                    { $set: { github_token: encryptedToken } },
+                    { upsert: true })
+                    .then(res => {
+                        robot.logger.info(`${username}'s GitHub Token Added to DB!`)
+                        robot.emit('refreshBrain') //refresh brain to update tokens       
+                    })
+                    .catch(err => {
+                        robot.logger.error(err)
+                        if (c.errorsChannel) {
+                            robot.messageRoom(c.errorsChannel, c.errorMessage
+                                + `Script: ${path.basename(__filename)}`)
+                        }
+                    })
+                    .done(() => {
+                        db.close()
+                    })
+            });
+        });
+        res.redirect('');
+    });
