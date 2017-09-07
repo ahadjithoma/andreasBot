cdce3513d3898bcd26292350afd50d06c3aa0aca
andreash92 <andreas_h92@outlook.com>
2017-09-07 04:43:41 +0300

    backing up wednesday

--- scripts/github-webhooks.js
+++ scripts/github-webhooks.js
@@ -2,6 +2,7 @@ var url = require('url');
 var querystring = require('querystring');
 var slackMsgs = require('./slackMsgs.js')
 var color = require('./colors.js')
+var Promise = require('bluebird')
 
 var githubURL = 'https://www.github.com/'
 
@@ -55,19 +56,144 @@ module.exports = function (robot) {
 			case 'pull_request':
 				pullRequestEvent(eventBody)
 				break
+			case 'pull_request_review':
+				pullRequestReviewEvent(eventBody)
+				break
+			case 'pull_request_review_comment':
+				pullRequestReviewCommentEvent(eventBody)
+				break
 			case 'fork':
 				break;
 			case 'pull':
 				break;
-			case '':
-				break;
-
 			default:
 				var room = eventBody.query.room
 				robot.messageRoom(room, `event: ${eventBody.eventType}`);
 				break;
 		}
 	}
+
+	function pullRequestReviewEvent(eventBody) {
+		var room = eventBody.query.room
+		var payload = eventBody.payload
+
+		// NOT HANDLING these type of ACTIONS.
+		// Delete them in case of handling them.
+		if (payload.action == 'edited' || payload.action == 'dismiss') {
+			return 0
+		}
+
+		var msg = { attachments: [] }
+		var attachment = slackMsgs.attachment()
+
+		var repoFullName = payload.repository.full_name
+		var repoURL = githubURL + repoFullName
+		var senderUsername = payload.sender.login
+		var senderURL = payload.sender.html_url
+		var pullReqNum = payload.pull_request.number
+		var pullReqURL = payload.pull_request.html_url
+		var pullReqTitle = payload.pull_request.title
+
+		var state = payload.review.state
+		switch (state) {
+			case 'commented':
+			case 'changes_requested':
+				state = state.split('_').reverse().join(' ')
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> <${senderURL}|${senderUsername}> ` +
+					`${bold(state)} on pull request <${pullReqURL}|#${pullReqNum}: ${pullReqTitle}> `
+				attachment.text = payload.review.body
+				attachment.color = 'warning'
+				msg.attachments.push(attachment)
+				break
+			case 'approved':
+				state = state.split('_').reverse().join(' ')
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> <${senderURL}|${senderUsername}> ` +
+					`${bold(state)} pull request <${pullReqURL}|#${pullReqNum}: ${pullReqTitle}> `
+				attachment.text = payload.review.body
+				attachment.color = 'good'
+				msg.attachments.push(attachment)
+				break
+		}
+		attachment.fallback = attachment.pretext
+
+		robot.messageRoom(room, msg)
+	}
+
+	function pullRequestReviewCommentEvent(eventBody) {
+		var room = eventBody.query.room
+		var payload = eventBody.payload
+
+		var msg = { attachments: [] }
+		var attachment = slackMsgs.attachment()
+
+		var repoFullName = payload.repository.full_name
+		var repoURL = githubURL + repoFullName
+		var refType = payload.ref_type //repo, branch or tag 
+		var refName = payload.ref
+		var refURL = repoURL + '/tree/' + refName
+		var senderUsername = payload.sender.login
+		var senderURL = payload.sender.html_url
+
+		var action = payload.action
+		switch (action) {
+			case 'created':
+
+				break
+		}
+	}
+
+	function issueCommentEvent(eventBody) {
+		var room = eventBody.query.room
+		var payload = eventBody.payload
+
+		var msg = { attachments: [] }
+		var attachment = slackMsgs.attachment()
+
+		var repoFullName = payload.repository.full_name
+		var repoURL = githubURL + repoFullName
+		var issueURL = payload.issue.html_url
+		var issueNum = payload.issue.number
+		var issueTitle = payload.issue.title
+		var issueType = ''
+		var senderUsername = payload.sender.login
+		var senderURL = payload.sender.html_url
+
+		var action = payload.action
+		switch (action) {
+			case 'created':
+
+				if (payload.issue.pull_request) {
+					issueType = 'pull request'
+				}
+				else {
+					issueType = 'issue'
+				}
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> ${bold('New comment')} ` +
+					`by <${senderURL}|${senderUsername}> on ${issueType} <${issueURL}|#${issueNum}: ${issueTitle}> `
+				attachment.text = payload.comment.body
+				attachment.color = color.getHex('gray')
+				msg.attachments.push(attachment)
+				attachment = slackMsgs.attachment()
+				attachment.color = color.getHex('blue')
+
+				if (payload.issue.assignees.length) {
+					attachment.fields.push({
+						title: 'Assignees:',
+						value: getUsersToString(payload.issue.assignees),
+						short: true
+					})
+				}
+				msg.attachments.push(attachment)
+
+				robot.messageRoom(room, msg)
+				break
+			case 'edited':
+			case 'deleted':
+				// TODO: is it usefull? 
+				break
+		}
+	};
+
 
 	function createAndDeleteEvent(eventBody) {
 		var room = eventBody.query.room
@@ -104,8 +230,116 @@ module.exports = function (robot) {
 		var room = eventBody.query.room
 		var payload = eventBody.payload
 
-		robot.messageRoom(room, 'TODO: pull request event')
+		var msg = { attachments: [] }
+		var attachment = slackMsgs.attachment()
+
+		var repoFullName = payload.repository.full_name
+		var repoURL = githubURL + repoFullName
+		var senderUsername = payload.sender.login
+		var senderURL = payload.sender.html_url
+		var pullReqNum = payload.number
+		var pullReqURL = payload.pull_request.html_url
+		var pullReqTitle = payload.pull_request.title
+		var pullReqDescr = payload.pull_request.body
+
+		var action = payload.action // PR's action
+		switch (action) {
+			case 'opened':
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> Pull request submitted by <${senderURL}|${senderUsername}>`
+				attachment.title = `<${pullReqURL}|#${pullReqNum} ${pullReqTitle}>`
+				attachment.text = pullReqDescr
+				var assignees = []
+				var reviewers = []
+				for (var i = 0; i < payload.pull_request.assignees.length; i++) {
+					assignees.push(payload.pull_request.assignees[i].login)
+				}
+				for (var i = 0; i < payload.pull_request.requested_reviewers.length; i++) {
+					reviewers.push(payload.pull_request.requested_reviewers[i].login)
+				}
+				if (assignees.length) {
+					attachment.fields.push({
+						title: 'Assignees:',
+						value: assignees.toString().replace(/,/g, ', '),
+						short: true
+					})
+				}
+				if (reviewers.length) {
+					attachment.fields.push({
+						title: 'Reviewers:',
+						value: reviewers.toString().replace(/,/g, ', '),
+						short: true
+					})
+				}
+				attachment.color = 'good'
+				break
+			case 'closed':
+			case 'reopened':
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> Pull request ${bold(action)} by <${senderURL}|${senderUsername}>`
+				attachment.title = `<${pullReqURL}|#${pullReqNum} ${pullReqTitle}>`
+				attachment.text = pullReqDescr
+				if (action == 'closed') {
+					attachment.color = 'danger'
+				}
+				else if (action == 'reopened') {
+					attachment.color = color.getHex('blue')
+				}
+				break
+			case 'edited':
+				var changedElement = ''
+				if (payload.changes.base) {
+					changedElement = ' the base branch'
+				}
+				else if (payload.changes.title) {
+					changedElement = ' the title'
+				}
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> <${senderURL}|${senderUsername}> `
+					+ `${bold(action + changedElement)} on pull request <${pullReqURL}|#${pullReqNum}: ${pullReqTitle}>`
+				break
+			case 'assigned':
+			case 'unassigned':
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> <${senderURL}|${senderUsername}> `
+					+ `${bold('changed assignees')} (${action}) on pull request <${pullReqURL}|#${pullReqNum}: ${pullReqTitle}>`
+				var assignees = []
+				for (var i = 0; i < payload.pull_request.assignees.length; i++) {
+					assignees.push(payload.pull_request.assignees[i].login)
+				}
+				if (assignees.length) {
+					attachment.fields.push({
+						title: 'Assignees:',
+						value: assignees.toString().replace(/,/g, ', '),
+						short: false
+					})
+				}
+				attachment.color = color.getHex('blue')
+				break
+			case 'review_requested':
+			case 'review_request_removed':
+				var actionText = action.split('_').reverse().toString().replace(/,/g, ' ')
+
+				attachment.pretext = `<${repoURL}|[${repoFullName}]> <${senderURL}|${senderUsername}> `
+					+ `${bold(actionText)} on pull request <${pullReqURL}|#${pullReqNum}: ${pullReqTitle}>`
+				var reviewers = []
+				for (var i = 0; i < payload.pull_request.requested_reviewers.length; i++) {
+					reviewers.push(payload.pull_request.requested_reviewers[i].login)
+				}
+				if (reviewers.length) {
+					attachment.fields.push({
+						title: 'Reviewers:',
+						value: reviewers.toString().replace(/,/g, ', '),
+						short: false
+					})
+				}
+				attachment.color = color.getHex('blue')
+				break
+			default:
+				robot.logger.info('pull request event action: ' + action + 'not handled')
+				break
+		}
+		msg.attachments.push(attachment)
+		robot.messageRoom(room, msg)
+
 	}
+
 
 	function pushEvent(eventBody) {
 		var room = eventBody.query.room
@@ -139,6 +373,10 @@ module.exports = function (robot) {
 		else if (payload.deleted) {
 			// TODO
 		}
+		else {
+			msg.text = `<${repo_url}|[${repo_name}:${branch}]> ${commits} new <${compare_url}|commit(s)> by <${user_url}|${user_name}>:`;
+		}
+
 		msg.attachments[0].color = '#0000ff'; // set color = blue
 		robot.messageRoom(room, msg);
 
@@ -226,11 +464,25 @@ module.exports = function (robot) {
 		}
 	};
 
-	function issueCommentEvent(eventBody) {
-		//TODO
-	};
 
 
+	function bold(text) {
+		if (robot.adapterName == 'slack') {
+			return `*${text}*`
+		}
+		// Add any other adapters here  
+		else {
+			return text
+		}
+	}
 
+	function getUsersToString(usersArray) {
+		var usersString = []
+		for (var i = 0; i < usersArray.length; i++) {
+			usersString.push(usersArray[i].login)
+		}
+		return usersString.toString().replace(/,/g, ', ')
+
+	}
 
 }
